da832224844aa7b87b2c88862e3c7c48602cb49f
Tuan Vu <tuan.vu@logigear.com>
2021-04-26 11:34:06 +0700

    move open()

--- src/main/java/page/BasePage.java
+++ src/main/java/page/BasePage.java
@@ -1,5 +1,18 @@
 package page;
 
-public abstract class BasePage {
+import common.Constant;
+import driver.DriverManager;
 
+public abstract class BasePage {
+    /**
+     * Open the TA Dashboard
+     *
+     * @return the home page
+     * @author tuan.vu
+     */
+    public HomePage open() {
+        DriverManager.getDriver().navigate().to(Constant.DASHBOARD_URL);
+        DriverManager.getDriver().manage().window().maximize();
+        return new HomePage();
+    }
 }
